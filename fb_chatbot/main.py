cbd7db93b53a2c2b38b6e69d75190e9f1119b334
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-03-05 12:02:36 -0600

    connection to dialog flow

--- fb_chatbot/main.py
+++ fb_chatbot/main.py
@@ -1,50 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 import json
+from bot import send_message
 
 app = Flask(__name__)
-
-def get_messaging(body):
-    entries = body.get("entry")  # Esto devuelve unsa colección
-    messaging = entries[0].get("messaging") # Esto es un dict
-    return messaging
-
-def get_sender_id(body):
-    messaging = get_messaging(body)
-    sender = messaging[0].get("sender")
-
-    return sender.get("id")
-def get_user_message(body):
-    messaging = get_messaging(body)
-    message = messaging[0].get("message")
-    return message.get("text")
-
-def send_message():
-    PAGE_ACCESS_TOKEN = "EAADAic8CH4cBAIYFa88rsBzvWNqPkYyXfwomoLGOX0yyHTRLP1bEkSlSOCuOWcgjY7xJ1hfZAuJGEZA8j2ZBbjfy3QZC2KHkm6ZCgwYk09Qg3e4B7uiKKWWFplaTCmQZB4nyhoZAlAtRsI6BNshJzq4SQe6ixrZAdlk7dyZCLgzWPRQZDZD"
-    fb_url = "https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_ACCESS_TOKEN
-    body = json.loads(request.data)
-    fbid = get_sender_id(body)
-    message = get_user_message(body) # obtener el mensaje del usuario y repetirselo
-    request_body = {
-        "messaging_type": "RESPONSE",
-        "recipient": {
-            "id": fbid
-        },
-        "message": {
-            "text": message
-        }
-    }
-    response = requests.post(fb_url, json=request_body)
-
-    if response.status_code > 399:
-        print("Hubo un error al responder el mensaje")
-        print(response.text)
-        return False
-    else:
-        print("todo bien")
-        return True
-
-
 
 @app.route("/fb_webhook", methods=["GET", "POST"])
 def fb_webhook():
@@ -62,6 +21,16 @@ def fb_webhook():
         else: 
             return "Fallamos"
 
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    data = json.loads(request.data)
+    age = int(data.get("results").get("age").get("value"))
+    if age >= 18:
+        response = {"approved": "true"}
+    else:
+        response = {"approved": "false"}
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(
